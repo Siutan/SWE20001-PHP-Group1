dc78f587c603b1ed6c890c4b2cccc55253b4d889
LiamJohns96 <89575079+LiamJohns96@users.noreply.github.com>
2021-09-03 12:40:21 +1000

    Update Inventory.js
    Matched fields from edit/input form to the tables

--- src/views/Inventory.js
+++ src/views/Inventory.js
@@ -28,8 +28,10 @@ function Inventory() {
                     <tr>
                       <th>Product Number</th>
                       <th>Product Name</th>
+                      <th>Product Group</th>
                       <th>Total Stock</th>
-                      <th>Stock Remaining</th>
+                      <th>Current Stock</th>
+                      <th>Product Description</th>
                       <th className="text-center">Price (EA)</th>
                     </tr>
                   </thead>
@@ -37,50 +39,64 @@ function Inventory() {
                     <tr>
                       <td>A-0001</td>
                       <td>product 1</td>
+                      <td>Group 1</td>
                       <td>1200</td>
                       <td>1000</td>
+                      <td>**A description of the product**</td>
                       <td className="text-center">$36,738</td>
                     </tr>
                     <tr>
                       <td>A-0001</td>
                       <td>Product 2</td>
+                      <td>Group 1</td>
                       <td>3000</td>
                       <td>1739</td>
+                      <td>**A description of the product**</td>
                       <td className="text-center">$23,789</td>
                     </tr>
                     <tr>
                       <td>A-0001</td>
                       <td>product 3</td>
+                      <td>Group 1</td>
                       <td>1000</td>
                       <td>332</td>
+                      <td>**A description of the product**</td>
                       <td className="text-center">$56,142</td>
                     </tr>
                     <tr>
                       <td>A-0001</td>
                       <td>Product 4</td>
+                      <td>Group 2</td>
                       <td>10,000</td>
                       <td>3564</td>
+                      <td>**A description of the product**</td>
                       <td className="text-center">$38,735</td>
                     </tr>
                     <tr>
                       <td>A-0001</td>
                       <td>Product 5</td>
+                      <td>Group 3</td>
                       <td>2800</td>
                       <td>239</td>
+                      <td>**A description of the product**</td>
                       <td className="text-center">$63,542</td>
                     </tr>
                     <tr>
                       <td>A-0001</td>
                       <td>Product 6</td>
+                      <td>Group 6</td>
                       <td>2800</td>
                       <td>485</td>
+                      <td>**A description of the product**</td>
                       <td className="text-center">$78,615</td>
                     </tr>
                     <tr>
                       <td>A-0001</td>
                       <td>Product 7</td>
+                      <td>Group 4</td>
                       <td>2800</td>
                       <td>504</td>
+                      <td>**A description of the product**</td>
                       <td className="text-center">$98,615</td>
                     </tr>
                   </tbody>
@@ -97,65 +113,81 @@ function Inventory() {
               <CardBody>
                 <Table className="tablesorter" responsive>
                   <thead className="text-primary">
-                    <tr>
-                      <th>Product Number</th>
-                      <th>Product Name</th>
-                      <th>Total Stock</th>
-                      <th>Stock Remaining</th>
-                      <th className="text-center">Price (EA)</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>A-0001</td>
-                      <td>product 1</td>
-                      <td>1200</td>
-                      <td>1000</td>
-                      <td className="text-center">$36,738</td>
-                    </tr>
-                    <tr>
-                      <td>A-0001</td>
-                      <td>Product 2</td>
-                      <td>3000</td>
-                      <td>1739</td>
-                      <td className="text-center">$23,789</td>
-                    </tr>
-                    <tr>
-                      <td>A-0001</td>
-                      <td>product 3</td>
-                      <td>1000</td>
-                      <td>332</td>
-                      <td className="text-center">$56,142</td>
-                    </tr>
-                    <tr>
-                      <td>A-0001</td>
-                      <td>Product 4</td>
-                      <td>10,000</td>
-                      <td>3564</td>
-                      <td className="text-center">$38,735</td>
-                    </tr>
-                    <tr>
-                      <td>A-0001</td>
-                      <td>Product 5</td>
-                      <td>2800</td>
-                      <td>239</td>
-                      <td className="text-center">$63,542</td>
-                    </tr>
-                    <tr>
-                      <td>A-0001</td>
-                      <td>Product 6</td>
-                      <td>2800</td>
-                      <td>485</td>
-                      <td className="text-center">$78,615</td>
-                    </tr>
-                    <tr>
-                      <td>A-0001</td>
-                      <td>Product 7</td>
-                      <td>2800</td>
-                      <td>504</td>
-                      <td className="text-center">$98,615</td>
-                    </tr>
-                  </tbody>
+                  <tr>
+                    <th>Product Number</th>
+                    <th>Product Name</th>
+                    <th>Product Group</th>
+                    <th>Total Stock</th>
+                    <th>Current Stock</th>
+                    <th>Product Description</th>
+                    <th className="text-center">Price (EA)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>A-0001</td>
+                    <td>product 1</td>
+                    <td>Group 1</td>
+                    <td>1200</td>
+                    <td>1000</td>
+                    <td>**A description of the product**</td>
+                    <td className="text-center">$36,738</td>
+                  </tr>
+                  <tr>
+                    <td>A-0001</td>
+                    <td>Product 2</td>
+                    <td>Group 1</td>
+                    <td>3000</td>
+                    <td>1739</td>
+                    <td>**A description of the product**</td>
+                    <td className="text-center">$23,789</td>
+                  </tr>
+                  <tr>
+                    <td>A-0001</td>
+                    <td>product 3</td>
+                    <td>Group 1</td>
+                    <td>1000</td>
+                    <td>332</td>
+                    <td>**A description of the product**</td>
+                    <td className="text-center">$56,142</td>
+                  </tr>
+                  <tr>
+                    <td>A-0001</td>
+                    <td>Product 4</td>
+                    <td>Group 2</td>
+                    <td>10,000</td>
+                    <td>3564</td>
+                    <td>**A description of the product**</td>
+                    <td className="text-center">$38,735</td>
+                  </tr>
+                  <tr>
+                    <td>A-0001</td>
+                    <td>Product 5</td>
+                    <td>Group 3</td>
+                    <td>2800</td>
+                    <td>239</td>
+                    <td>**A description of the product**</td>
+                    <td className="text-center">$63,542</td>
+                  </tr>
+                  <tr>
+                    <td>A-0001</td>
+                    <td>Product 6</td>
+                    <td>Group 6</td>
+                    <td>2800</td>
+                    <td>485</td>
+                    <td>**A description of the product**</td>
+                    <td className="text-center">$78,615</td>
+                  </tr>
+                  <tr>
+                    <td>A-0001</td>
+                    <td>Product 7</td>
+                    <td>Group 4</td>
+                    <td>2800</td>
+                    <td>504</td>
+                    <td>**A description of the product**</td>
+                    <td className="text-center">$98,615</td>
+                  </tr>
+                </tbody>
                 </Table>
               </CardBody>
             </Card>
